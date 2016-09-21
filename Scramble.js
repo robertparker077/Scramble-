b492cb50102a3508928c473bdedcd0ba9d00eed5
Aileen Santos <asantos3026@gmail.com>
2016-09-21 16:02:04 -0700

    added characters and spacecraft data to game

--- Scramble.js
+++ Scramble.js
@@ -1,4 +1,4 @@
-const dictionary = {
+const actor = {
   "Mark Hamil": "Luke Skywalker",
   "Harrison Ford": "Han Solo",
   "Carrie Fisher": "Princess Leia",
@@ -10,14 +10,80 @@ const dictionary = {
   "Felicity Jones": "Jyn Erso",
 }
 
+const character = { "name": "4-LOM",
+ "name": "Aayla Secura",
+  "name": "Admiral Ackbar",
+  "name": "Admiral Thrawn",
+  "name": "Ahsoka Tano",
+  "name": "Anakin Solo",
+  "name": "Asajj Ventress",
+  "name": "Aurra Sing",
+  "name": "Senator Bail Organa",
+  "name": "Barriss Offee",
+  "name": "Bastila Shan",
+  "name": "Ben Skywalker",
+  "name": "Bib Fortuna",
+  "name": "Biggs Darklighter",
+  "name": "Boba Fett",
+  "name": "Bossk"
+}
+
+const spacecrafts = {
+ "name": "Corellian Corvette",
+ "name": "Death Star",
+ "name": "Ebon Hawk",
+"name": "Geonosian solar sailer",
+"name": "Imperial Landing Craft",
+"name": "Lambda-class shuttle",
+"name": "Millennium Falcon",
+"name": "Moldy Crow",
+"name": "Naboo royal cruiser",
+"name": "Naboo royal starship",
+"name": "Naboo star skiff",
+"name": "Nebulon-B frigate",
+"name": "Neimoidian shuttle",
+"name": "Outrider",
+"name": "Radiant VII",
+"name": "Raven's Claw",
+"name": "Rebel blockade runner",
+"name": "Republic assault ship",
+"name": "Republic attack cruiser",
+"name": "Republic cruiser",
+"name": "Rogue Shadow",
+"name": "Sith Infiltrator",
+"name": "Slave I",
+"name": "Star Destroyer",
+"name": "Starfreighter",
+"name": "Tantive IV",
+"name": "Techno Union Starship",
+"name": "Theta-class shuttle",
+"name": "Lucrehulk-class battleship",
+"name": "Trade Federation cruiser",
+"name": "Trade Federation landing ship",
+"name": "Virago",
+"name": " TIE Fighter"
+}
+
+const chooseRandomShip = function() {
+  const shipNames = Object.value( spacecrafts )
+
+  return characterNames[ Math.floor( Math.random() * characterNames.length )]
+}
+
+const chooseRandomCharacter = function() {
+  const characterNames = Object.value( character )
+
+  return characterNames[ Math.floor( Math.random() * characterNames.length )]
+}
+
 const chooseRandomActor = function() {
-  const actorNames = Object.keys( dictionary )
+  const actorNames = Object.keys( actor )
 
   return actorNames[ Math.floor( Math.random() * actorNames.length ) ]
 }
 
 const getCharacter = function( actorName ) {
-  const character = dictionary[ actorName ]
+  const character = actor[ actorName ]
 
   return document.getElementById('hint').innerHTML = character;
 }
@@ -62,14 +128,17 @@ const scoreKeeper = (function() {
 
 
 //DOM EVENTS
-$(document).ready( function() {
+
+const getActor = (function() {
   let lastActor = chooseRandomActor()
+  // let lastCharacter = chooseRandomCharacter()
+  // let lastShip = chooseRandomShip()
   let lastScore = 0
 
-  $('#random-word-1').text( shuffle( lastActor ) )
+  $('#random-actor').text( shuffle( lastActor ) )
 
   $( '#rescramble' ).click( function( event ) {
-    $('#random-word-1').text( shuffle( lastActor ) )    
+    $('#random-actor').text( shuffle( lastActor ) )    
   })
 
   $( '#am-i-right' ).click( function( event ) {
@@ -83,8 +152,15 @@ $(document).ready( function() {
       lastScore = score
       
       lastActor = chooseRandomActor()
-      $('#random-word-1').text( shuffle( lastActor ) ) 
+      $('#random-actor').text( shuffle( lastActor ) ) 
     }
-
   })
 })
+
+
+
+$(document).ready( function() {
+  getActor()
+})
+
+
